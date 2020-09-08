46a742527aaebf98e58470837e75c810d3af4426
martdmc <mart_hucherko@yahoo.com>
2020-09-08 09:04:32 +0200

    gemification

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,12 +2,12 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
-  #has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }
+  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }
 
-  #validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
+  validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
 
   def tag_list
-    tags.join(', ')
+    self.tags.collect{ |tag| tag.name }.join(", ")
   end
 
   def tag_list=(tags_string)
